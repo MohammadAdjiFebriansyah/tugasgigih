6c63423e9aac64f4e16e37fa8519463176c6826d
MohammadAdjiFebriansyah <adji280201@gmail.com>
2022-04-04 06:48:05 +0700

    homework modul 3 sesion 3

---
+++ homework-adji/src/Container/index.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import Tracks from '../components/Tracks';
+import SearchBar from '../components/SearchBar';
+import config from '../lib/config';
+import Button from '../components/Button';
+import CreatePlaylist from '../components/CreatePlaylist';
+import { getUserProfile } from '../lib/fetchApi.js';
+import { toast } from 'react-toastify';
+import "./index.css"
+
+export default function Home() {
+  const [accessToken, setAccessToken] = useState('');
+  const [isAuthorize, setIsAuthorize] = useState(false);
+  const [tracks, setTracks] = useState([]);
+  const [selectedTracksUri, setSelectedTracksUri] = useState([]);
+  const [selectedTracks, setSelectedTracks] = useState([]);
+  const [isInSearch, setIsInSearch] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const accessTokenParams = new URLSearchParams(window.location.hash).get('#access_token');
+
+    if (accessTokenParams !== null) {
+      setAccessToken(accessTokenParams);
+      setIsAuthorize(accessTokenParams !== null);
+
+      const setUserProfile = async () => {
+        try {
+          const response = await getUserProfile(accessTokenParams);
+
+          setUser(response);
+        } catch (e) {
+          toast.error(e);
+        }
+      }
+
+      setUserProfile();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isInSearch) {
+      setTracks(selectedTracks);
+    }
+  }, [selectedTracksUri]);
+
+  const getSpotifyLinkAuthorize = () => {
+    const clientId = "c3674ca69601470ebbf98c96ec75c3bd";
+    return `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:3000/`;
+  }
+
+  const onSuccessSearch = (searchTracks) => {
+    setIsInSearch(true);
+    const selectedSearchTracks = searchTracks.filter((track) => selectedTracksUri.includes(track.uri));
+    setTracks([...new Set([...selectedSearchTracks, ...searchTracks])])
+  }
+
+
+  const clearSearch = () => {
+    setTracks(selectedTracks);
+    setIsInSearch(false);
+  }
+
+
+  const toggleSelect = (track) => {
+    const uri = track.uri;
+
+    if (selectedTracksUri.includes(uri)) {
+      setSelectedTracksUri(selectedTracksUri.filter((item) => item !== uri));
+      setSelectedTracks(selectedTracks.filter((item) => item.uri !== uri));
+    } else {
+      setSelectedTracksUri([...selectedTracksUri, uri]);
+      setSelectedTracks([...selectedTracks, track]);
+    }
+  }
+
+  return (
+    <>
+      {!isAuthorize && (
+        <main className="center">
+          <p>Login your account</p>
+          <Button className='btn-login' href={getSpotifyLinkAuthorize()}>Login</Button>
+        </main>
+      )}
+
+      {isAuthorize && (
+        <main className="container" id="home">
+          <CreatePlaylist
+            accessToken={accessToken}
+            userId={user.id}
+            uriTracks={selectedTracksUri}
+          />
+          <hr/>
+
+          <SearchBar
+            accessToken={accessToken}
+            onSuccess={(tracks) => onSuccessSearch(tracks)}
+            onClearSearch={clearSearch}
+          />
+
+          <div className="content">
+            {tracks.length === 0 && (
+              <p>No tracks</p>
+            )}
+
+            <div className="tracks">
+              {tracks.map((track) => (
+                <Tracks
+                  key={track.id}
+                  imageUrl={track.album.images[0].url}
+                  title={track.name}
+                  artist={track.artists[0].name}
+                  select={selectedTracksUri.includes(track.uri)}
+                  toggleSelect={() => toggleSelect(track)}
+                />
+              ))}
+            </div>
+          </div>
+        </main>
+      )}
+    </>
+  );
+}
---
+++ homework-adji/src/components/Tracks/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './index.css';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+
+export default function Track({ imageUrl, title, artist, toggleSelect }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleToggleSelect = () => {
+    setIsSelected(!isSelected);
+    toggleSelect();
+  }
+
+  return (
+    <div className="song">
+      <div className="songImage">
+        <img src={imageUrl} alt={title} />
+      </div>
+
+      <div className="songData">
+          <h3 className="songTitle">{title}</h3>
+          <p className="songArtist">{artist}</p>
+      </div>
+      <div className="songAction">
+          <Button className='btn' variant={isSelected ? 'primary' : 'secondary'} onClick={handleToggleSelect}>{isSelected ? 'Deselect' : 'Select'}</Button>
+      </div>
+    </div>
+  );
+}
+
+Track.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  artist: PropTypes.string.isRequired,
+  toggleSelect: PropTypes.func.isRequired,
+}
